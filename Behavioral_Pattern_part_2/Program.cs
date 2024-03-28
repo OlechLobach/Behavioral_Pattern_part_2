4511cc094cda4595dff7a130a157543d0adc35c8
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-03-28 02:19:34 +0200

    initiall commit

--- Behavioral_Pattern_part_2/Program.cs
+++ Behavioral_Pattern_part_2/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Behavioral_Pattern_part_2
+﻿using ChatRoom;
+
+namespace MainProject
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ChatRoom.ChatRoom chatRoom = new ChatRoom.ChatRoom();
+
+            ChatParticipant participant1 = new ChatParticipant("Alice");
+            ChatParticipant participant2 = new ChatParticipant("Bob");
+            ChatParticipant participant3 = new ChatParticipant("Charlie");
+
+            chatRoom.Attach(participant1);
+            chatRoom.Attach(participant2);
+            chatRoom.Attach(participant3);
+
+            chatRoom.SendMessage("Hello, everyone!");
+
+            chatRoom.Detach(participant2);
+
+            chatRoom.SendMessage("Bob left the chat.");
+
+            chatRoom.SendMessage("Charlie, did you hear that?");
         }
     }
 }
---
+++ Pattern/Observer/Chatroom.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatRoom
+{
+    public interface IObserver
+    {
+        void Update(string message);
+    }
+
+    public interface ISubject
+    {
+        void Attach(IObserver observer);
+        void Detach(IObserver observer);
+        void Notify(string message);
+    }
+
+    public class ChatRoom : ISubject
+    {
+        private List<IObserver> observers = new List<IObserver>();
+
+        public void Attach(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void Notify(string message)
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update(message);
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            Console.WriteLine($"Sending message: {message}");
+            Notify(message);
+        }
+    }
+
+    public class ChatParticipant : IObserver
+    {
+        private string name;
+
+        public ChatParticipant(string name)
+        {
+            this.name = name;
+        }
+
+        public void Update(string message)
+        {
+            Console.WriteLine($"{name} received message: {message}");
+        }
+    }
+}
